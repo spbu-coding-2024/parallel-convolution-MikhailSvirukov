921c36a21de30c6e634c0b4e066c5372b300b4ff
MikhailSvirukov <svmk17@mail.ru>
2026-04-30 15:02:04 +0300

    feat: build config

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.13"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
+    implementation(files("libs/opencv-490.jar"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.16")
+}
+
+val libPath = "$rootDir/libs"
+
+tasks.withType<JavaExec> {
+    jvmArgs("-Djava.library.path=$libPath")
+}
+
+tasks.withType<Test> {
+    jvmArgs("-Djava.library.path=$libPath")
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.example.MainKt")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+ktlint {
+    version = "1.4.0"
+}
